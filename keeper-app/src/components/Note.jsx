5be5e2b312df4990d7d10b7a6369c4984a5e8e81
Webstar Maseko <webstarsiyabonga@gmail.com>
2021-03-05 06:53:31 +0200

    changed image url

--- keeper-app/src/components/Note.jsx
+++ keeper-app/src/components/Note.jsx
@@ -16,7 +16,7 @@ function Note(props) {
      
       
       <div className="note" style={bg} onMouseEnter={() =>setVis(true)} onMouseLeave={() =>setVis(false)} >
-      {props.img === undefined ? null: <img src={`build/ext-images/${props.img}`} alt=""/>}
+      {props.img === undefined ? null: <img src={`keeper-app/build/ext-images/${props.img}`} alt=""/>}
 
         <h1>{props.title}</h1>
         <p>{props.content}</p>
