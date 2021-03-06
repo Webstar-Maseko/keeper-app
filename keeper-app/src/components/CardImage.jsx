e5d1e5620ce247729a1b82e70b97d544a3e95981
Webstar Maseko <webstarsiyabonga@gmail.com>
2021-03-06 14:32:20 +0200

    added device meta

--- keeper-app/src/components/CardImage.jsx
+++ keeper-app/src/components/CardImage.jsx
@@ -1,14 +1,10 @@
 import Card  from "react-bootstrap/Card";
 import CardGroup from "react-bootstrap/CardGroup";
-import Button from "react-bootstrap/Button";
-
-
-
  function CardImage(props)
  {
     return(<CardGroup >
             <Card className="home-card" >
-                <Card.Img src={`${process.env.PUBLIC_URL}/assets/images/${props.img}`}/>
+                <Card.Img src={`./assets/images/${props.img}`}/>
             </Card>
             <Card  body className="text-left tb text-white home-card" >
             <h4>
